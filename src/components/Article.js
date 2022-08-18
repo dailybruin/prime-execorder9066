01029523730ed3f255dbfa3761365eabc7d9854b
Tracy Zhao <tracyzhao24@g.ucla.edu>
2022-08-17 21:14:00 -0700

    header of article

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,6 +6,7 @@ const Container = styled.div`
   margin: 12% auto 10% auto;
   width: 80%;
   height: fit-content;
+  text-align: left;
 `;
 
 
@@ -14,7 +15,22 @@ const Content = styled.div`
     font-size: 20px;
     line-height: 24px;
     margin: auto;
-    text-align: left;
+`;
+
+
+const Title = styled.h2`
+    font-size: 64px;
+`;
+
+const Byline = styled.div`
+    font-size: 24px;
+    line-height: 60px;
+`;
+
+const Date = styled.div`
+    font-size: 24px;
+    line-height: 60px;
+    margin-bottom: 30px;
 `;
 
 
@@ -23,7 +39,16 @@ export default function Article(props) {
       <Container>
         <Content style={{ margin: "auto" }}>
             {props.main.map((info) => {
-                if (info.type == "text") {
+                if (info.type == "header")
+                {
+                  return (
+                    <div>
+                      <Title> {info.value.article_title} </Title>
+                      <Byline> {info.value.article_byline} </Byline>
+                      <Date> {info.value.article_date} </Date>
+                    </div>
+                  )
+                } else if (info.type == "text") {
                   return <p>{info.value}</p>;
                 } else if (info.type == "pull") {
                   return <q>{info.content}</q>;
